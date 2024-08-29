01814b5d2734ae2bb07c76a032c8457004663dac
plafet <plafett@gmail.com>
2024-08-29 21:20:34 +0300

    скорректировали комментарий для скрипта систем инфо

--- lesson_3/script_sistem_info.sh
+++ lesson_3/script_sistem_info.sh
@@ -1,5 +1,5 @@
 sudo lshw -class processor && echo "инфа о проце" \
 && cat /proc/meminfo && echo "инфа о памяти из файла" \
 && lsblk -o name,size,fstype,mountpoint && echo "блочные устройства, размер диска" \
-&& hostnamectl && echo "чекнем версию системы, ядра и имя машины" \
+&& hostnamectl && echo "чекнули версию системы, ядра и имя машины" \
 && echo "Ура! мы все выяснили. давай до свидания" 
